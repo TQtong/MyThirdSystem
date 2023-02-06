e093ad91cbed91ed892168ed598939ab4107b519
TQ <tangqi@southzn.com>
2023-02-06 14:40:30 +0800

    优化请求体

--- CreateNotbookSystem.NavigationBar/Service/BaseRequest.cs
+++ CreateNotbookSystem.NavigationBar/Service/BaseRequest.cs
@@ -14,7 +14,6 @@ namespace CreateNotbookSystem.NavigationBar.Service
     {
         public Method Method { get; set; }
         public string Route { get; set; }
-        public string ContentType { get; set; } = "application/json";
         public object Parameter { get; set; }
     }
 }
